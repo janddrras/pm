b57a618967ebd9996702d268abf97dc7810e5ae4
janddrras <janddrras@gmail.com>
2024-02-29 16:45:55 +0200

    login ui

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import Link from "next/link"
+
+const Signup = () => {
+  return (
+    <div className="mt-24 rounded bg-card border border-accent py-10 px-6 md:px-14 md:max-w-sm md:mt-0">
+      <form action="">
+        <h1 className="text-3xl font-semibold text-center">Sign Up</h1>
+        <div className="space-y-6 mt-10">
+          <Input type="email" name="email" placeholder="Email" />
+          <Button type="submit" className="w-full">
+            Sign Up
+          </Button>
+        </div>
+      </form>
+      <div className="mt-6 flex text-sm">
+        <p className="text-gray-600 mr-2">Already have an account?</p>
+        <Link className="hover:underline" href="/login">
+          Sign In
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
